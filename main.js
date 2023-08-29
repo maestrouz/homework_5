0132b6cc50c3a72efd1a34ca0fafbbf7c756b438
maestrouz <tsrichpubgm@gmail.com>
2023-08-30 04:22:20 +0500

    hw5

---
+++ main.js
@@ -0,0 +1,239 @@
+//   1
+// let h1 = document.getElementsByTagName('h1');
+// let h1_class = document.getElementsByClassName('h1');
+// let h1_id = document.getElementById('h1');
+// h1[0].textContent = 'Salom'
+
+//   2
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2_id[0].textContent = 'Mercedes'
+
+//   3
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2_class[0].textContent = 'Iphone'
+
+//   4
+// let h2 = document.getElementsByClassName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'Uzbekistan'
+
+//   5
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'Tree'
+
+//   6
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'ipsum'
+
+//   7
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'Asus'
+
+//   8 
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'Earth'
+
+//   9
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2_class[0].textContent = 'JavaScript'
+
+//   10
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'qalam'
+
+//   11
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'Daftar'
+
+//   12
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'Kun'
+
+//   13
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2_class[0].textContent = 'Osmon'
+
+//   14
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'Olov'
+
+//   15
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2_class[0].textContent = 'daryo'
+
+//   16
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'qora'
+
+//   17
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'stul'
+
+//   18
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2_class[0].textContent = 'tanga'
+
+//   19
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'explore'
+
+//   20
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2_class[0].textContent = 'Ilm'
+
+//   21
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'seshanba'
+
+//   22
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2_class[0].textContent = 'chorshanba'
+
+//   23
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'payshanba'
+
+//   24
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'juma'
+
+//   25
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'shanba'
+
+//   26
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'yakshanba'
+
+//   27
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'burun'
+
+//   28
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2_class[0].textContent = 'devor'
+
+//   29
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2_class[0].textContent = 'qahva'
+
+//   30
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2_class[0].textContent = 'kichik'
+
+//   31
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2_class[0].textContent = 'kalta'
+
+//   32
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2_class[0].textContent = 'anor'
+
+//   33
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'sovuq'
+
+//   34
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = '3G'
+
+//   35
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'ikki'
+
+//   36
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'oyoq'
+
+//   37
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'uka'
+
+//   38
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'ona'
+
+//   39
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'vaqt'
+
+//   40
+// let h2 = document.getElementsByTagName('h2');
+// let h2_class = document.getElementsByClassName('h2');
+// let h2_id = document.getElementById('h2');
+// h2[0].textContent = 'uzavto'
